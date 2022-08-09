e2e35977bfd49a12a8745321b1b5ef9f9d61997a
Verity Collins <verity.carlos@gmail.com>
2022-08-09 15:57:29 -0500

    commit tree 3bfcf8e0b51a9347b60da1add5e2030945e97309

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,15 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  const bio=props.bio
   return (
+   
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bio ? <p>{bio}</p> : null }
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.github} linkedin={props.linkedin}/>
     </div>
   );
 }
